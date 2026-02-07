3666048e69a704215fc497bf78bf3ccde9772943
danielgarcia-j <00014325@uca.edu.sv>
2026-02-07 01:49:33 -0600

    feat:Buscar estudiante por carnet

--- registro.cpp
+++ registro.cpp
@@ -1,59 +1,16 @@
- feature/mostrar-estudiantes
-
-
-#include <iostream>
-
-using namespace std;
-
-struct Estudiante {
-    int carnet;
-    char nombre[50];
-    float nota;
-    Estudiante* izquierdo;
-    Estudiante* derecho;
-};
-
-Estudiante* crearEstudiante(int carnet, char nombre[], float nota) {
-    Estudiante* nuevo = new Estudiante();
-    nuevo->carnet = carnet;
-    nuevo->nota = nota;
-    nuevo->izquierdo = NULL;
-    nuevo->derecho = NULL;
-
-    int i = 0;
-    while (nombre[i] != '\0' && i < 49) {
-        nuevo->nombre[i] = nombre[i];
-        i++;
-    }
-    nuevo->nombre[i] = '\0';
-
-    return nuevo;
-}
-
-Estudiante* insertar(Estudiante* raiz, int carnet, char nombre[], float nota) {
+void buscarEstudiante(Estudiante* raiz, int carnet) {
     if (raiz == NULL) {
-        return crearEstudiante(carnet, nombre, nota);
+        cout << "Estudiante no encontrado\n";
+        return;
     }
 
-    if (carnet < raiz->carnet) {
-        raiz->izquierdo = insertar(raiz->izquierdo, carnet, nombre, nota);
-    } else if (carnet > raiz->carnet) {
-        raiz->derecho = insertar(raiz->derecho, carnet, nombre, nota);
-    }
- develop
-
-    return raiz;
-}
-
- feature/mostrar-estudia
-void mostrarEstudiantes(Estudiante* raiz) {
-    if (raiz != NULL) {
-        mostrarEstudiantes(raiz->izquierdo);
+    if (carnet == raiz->carnet) {
         cout << "Carnet: " << raiz->carnet
              << " Nombre: " << raiz->nombre
              << " Nota: " << raiz->nota << endl;
-        mostrarEstudiantes(raiz->derecho);
+    } else if (carnet < raiz->carnet) {
+        buscarEstudiante(raiz->izquierdo, carnet);
+    } else {
+        buscarEstudiante(raiz->derecho, carnet);
     }
 }
-
- develop
