15d36f547c98df8cc4bc85a9778e2de17c1f19d2
Jeffrey Guevara <00388525@uca.edu.sv>
2026-02-07 12:39:36 -0600

    add: aprobados y reprobados

--- registro.cpp
+++ registro.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-
-
- return  0;   
+void mostrarAprobados(Estudiante* raiz) {
+    if (raiz != NULL) {
+        mostrarAprobados(raiz->izquierdo);
+        if (raiz->nota >= 6.0) {
+            cout << raiz->nombre << " - " << raiz->nota << endl;
+        }
+        mostrarAprobados(raiz->derecho);
+    }
 }
+
+void mostrarReprobados(Estudiante* raiz) {
+    if (raiz != NULL) {
+        mostrarReprobados(raiz->izquierdo);
+        if (raiz->nota < 6.0) {
+            cout << raiz->nombre << " - " << raiz->nota << endl;
+        }
+        mostrarReprobados(raiz->derecho);
+    }
+}
+
