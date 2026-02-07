e2a627d61cd784f708acb35072c0f74f952b900e
danielgarcia-j <00014325@uca.edu.sv>
2026-02-07 01:10:10 -0600

    feat:Insertar estudiante por carnet

--- registro.cpp
+++ registro.cpp
@@ -1,8 +1,43 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main() {
+struct Estudiante {
+    int carnet;
+    char nombre[50];
+    float nota;
+    Estudiante* izquierdo;
+    Estudiante* derecho;
+};
 
+Estudiante* crearEstudiante(int carnet, char nombre[], float nota) {
+    Estudiante* nuevo = new Estudiante();
+    nuevo->carnet = carnet;
+    nuevo->nota = nota;
+    nuevo->izquierdo = NULL;
+    nuevo->derecho = NULL;
 
- return  0;   
+    int i = 0;
+    while (nombre[i] != '\0' && i < 49) {
+        nuevo->nombre[i] = nombre[i];
+        i++;
+    }
+    nuevo->nombre[i] = '\0';
+
+    return nuevo;
 }
+
+Estudiante* insertar(Estudiante* raiz, int carnet, char nombre[], float nota) {
+    if (raiz == NULL) {
+        return crearEstudiante(carnet, nombre, nota);
+    }
+
+    if (carnet < raiz->carnet) {
+        raiz->izquierdo = insertar(raiz->izquierdo, carnet, nombre, nota);
+    } else if (carnet > raiz->carnet) {
+        raiz->derecho = insertar(raiz->derecho, carnet, nombre, nota);
+    }
+
+    return raiz;
+}
+
